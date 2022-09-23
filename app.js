beff4d8839e68da6890b15431250e04758e2cdc1
David Hsieh <yen888yen888@gmail.com>
2022-09-22 21:46:07 -0700

    Bubble color hue rotate

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = [" solve problems.", " face challenges.", " bridge connections."];
+const textArray = [
+  " solve problems.",
+  " face challenges.",
+  " bridge connections.",
+];
 const typingDelay = 150;
 const erasingDelay = 80;
 const newTextDelay = 1500; // Delay between current and next text
@@ -11,12 +15,12 @@ let charIndex = 0;
 // for intro page
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
-  }
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
     setTimeout(erase, newTextDelay);
   }
@@ -24,12 +28,15 @@ function type() {
 
 function erase() {
   if (charIndex > 0) {
-    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
     charIndex--;
     setTimeout(erase, erasingDelay);
-  }
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
     textArrayIndex++;
     if (textArrayIndex >= textArray.length) textArrayIndex = 0;
@@ -37,8 +44,8 @@ function erase() {
   }
 }
 
-
-document.addEventListener("DOMContentLoaded", () => { // On DOM Load initiate the effect
+document.addEventListener("DOMContentLoaded", () => {
+  // On DOM Load initiate the effect
   if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
@@ -55,5 +62,26 @@ document.addEventListener("scroll", () => {
 
 document.getElementById("to-top-button").addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
+});
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const anchor = document.getElementById("anchor");
+  const rekt = anchor.getBoundingClientRect();
+  const anchorX = rekt.left + rekt.width / 2;
+  const anchorY = rekt.top + rekt.height / 2;
+
+  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+  anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+});
+
+function angle(cx, cy, ex, ey) {
+  const dy = ey - cy;
+  const dx = ex - cx;
+  const rad = Math.atan2(dy, dx); // range (-PI, PI]
+  const deg = (rad * 180) / Math.PI; // rads to degs, range (-180, 180]
+  return deg;
 }
-);
