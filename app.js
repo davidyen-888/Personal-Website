23cb6a681d6d7e0661ea43cb10699b2698c362d5
David Hsieh <yen888yen888@gmail.com>
2023-02-06 20:41:31 -0800

    Disable bubble effect

--- app.js
+++ app.js
@@ -64,24 +64,24 @@ document.getElementById("to-top-button").addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
 });
 
-document.addEventListener("mousemove", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
+// document.addEventListener("mousemove", (e) => {
+//   const mouseX = e.clientX;
+//   const mouseY = e.clientY;
 
-  const anchor = document.getElementById("anchor");
-  const rekt = anchor.getBoundingClientRect();
-  const anchorX = rekt.left + rekt.width / 2;
-  const anchorY = rekt.top + rekt.height / 2;
+//   const anchor = document.getElementById("anchor");
+//   const rekt = anchor.getBoundingClientRect();
+//   const anchorX = rekt.left + rekt.width / 2;
+//   const anchorY = rekt.top + rekt.height / 2;
 
-  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+//   const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
 
-  anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
-});
+//   anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+// });
 
-function angle(cx, cy, ex, ey) {
-  const dy = ey - cy;
-  const dx = ex - cx;
-  const rad = Math.atan2(dy, dx); // range (-PI, PI]
-  const deg = (rad * 180) / Math.PI; // rads to degs, range (-180, 180]
-  return deg;
-}
+// function angle(cx, cy, ex, ey) {
+//   const dy = ey - cy;
+//   const dx = ex - cx;
+//   const rad = Math.atan2(dy, dx); // range (-PI, PI]
+//   const deg = (rad * 180) / Math.PI; // rads to degs, range (-180, 180]
+//   return deg;
+// }
